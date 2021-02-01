d74598dc89550d043a39daa2c4661101db2eeb21
ony <onydarrell@gmail.com>
2021-02-01 12:51:53 +0700

    Update tes.js

--- tes/tes.js
+++ tes/tes.js
@@ -2,17 +2,17 @@ const vis = document.getElementsByClassName("sound-vis")[0];
 const state = document.getElementsByClassName("state")[0];
 document.body.addEventListener("click", () => {
   if (vis.className.indexOf("audio-playing") < 0) {
-    vis.classList.remove("audio-paused");
+    // vis.classList.remove("audio-paused");
     vis.classList.add("audio-playing");
 
-    state.classList.remove("paused");
-    state.classList.add("playing");
-  } else {
-    vis.classList.remove("audio-playing");
-    vis.classList.add("audio-paused");
+    // state.classList.remove("paused");
+    // state.classList.add("playing");
+  // } else {
+  //   vis.classList.add("audio-paused");
+  //   vis.classList.remove("audio-playing");
 
-    state.classList.add("playing");
-    state.classList.remove("paused");
+  //   state.classList.add("paused");
+  //   state.classList.remove("playing");
   }
 });
 
