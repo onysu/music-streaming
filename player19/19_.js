353b82288dd12d9c774194142452df96384b9fd3
ony <onydarrell@gmail.com>
2021-02-28 05:44:33 +0700

    U

--- player19/19_.js
+++ player19/19_.js
@@ -1,27 +1,27 @@
 $('#playlist').musicPlaylist({
   // Opsi Utama
   playerWidth: '100%', // Lebar player. Atur dalam persen (%) untuk membuatnya responsif atau dalam piksel (px) untuk membuatnya tetap lebar
-  playerColor: '#000', // Warna latar belakang pemutar
+  playerColor: '#eee', // Warna latar belakang pemutar
   playerOpacity: '1', // Opasitas latar belakang pemutar dan daftar putar: 0,1 - 1
-  iconColor: '#999', // Warna ikon player
-  slidersBackgroundColor: '#333333', // Warna latar slider waktu dan volume
-  slidersValueColor: '#FF0000', // Penggeser waktu dan volume warna aktif
-  playerSize: 'small', // Melacak ukuran karya seni: big, small
-  titleSize: '15px', // Melacak ukuran judul
-  titleColor: '#fff', // Warna judul lagu
-  titleFontFamily: 'arial', // Melacak font judul
+  iconColor: '#333', // Warna ikon player
+  slidersBackgroundColor: '#ddd', // Warna latar slider waktu dan volume
+  slidersValueColor: 'red', // Penggeser waktu dan volume warna aktif
+  playerSize: 'small', // ukuran karya seni: big, small
+  titleSize: '15px', // ukuran judul
+  titleColor: '#222', // Warna judul lagu
+  titleFontFamily: 'arial', // font judul
   startingVolume: '100', // Volume player awal: 0 - 100
 
   // Opsi Daftar Putar
   showPlayList: 'show', // Tunjukkan playlist pada awalnya: tampilkan, sembunyikan
-  trackImageSize: '40px', // Melacak ukuran karya seni dalam playlist
-  trackTextColor: '#fff', // Melacak warna judul dalam playlist
-  trackTextSize: '14px', // Melacak ukuran judul dalam playlist
-  trackFontFamily: 'arial', // Melacak font judul dalam playlist
-  playlistColor: 'blue', // Warna latar daftar putar
-  activeTrackBackground: '#525252', // Track aktif dalam warna playlist
-  activeTrackTextColor: '#fff', // Warna judul track aktif dalam playlist
-  separatorColor: '#333', // Melacak warna pemisah
+  trackImageSize: '30px', // ukuran foto dalam playlist
+  trackTextColor: '#111', // warna judul dalam playlist
+  trackTextSize: '12px', // ukuran judul dalam playlist
+  trackFontFamily: 'arial', // font judul dalam playlist
+  playlistColor: '#999', // Warna latar daftar putar
+  activeTrackBackground: '#333', // Track aktif dalam warna playlist
+  activeTrackTextColor: 'yellow', // Warna judul track aktif dalam playlist
+  separatorColor: '#333', // warna pemisah
 });
 
 // <![CDATA[  <-- For SVG support
@@ -50,10 +50,10 @@ if ('WebSocket' in window) {
       else if (msg.data == 'refreshcss') refreshCSS();
     };
     if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
-      console.log('Live reload enabled.');
+      console.log('Reload langsung diaktifkan.');
       sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
     }
   })();
 } else {
-  console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+  console.error('Tingkatkan versi browser Anda. Browser ini TIDAK mendukung WebSocket untuk Live-Reloading.');
 }
