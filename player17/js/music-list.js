611bf059708597df815dae391c1e89d000a8e2dd
ony <onydarrell@gmail.com>
2021-07-11 04:34:06 +0700

    M

--- player17/js/music-list.js
+++ player17/js/music-list.js
@@ -1,61 +1,61 @@
 let allMusic = [{
-        name: "Bring Me The Horizon - Drown",
+        name: "Drown",
         artist: "Bring Me The Horizon",
-        img: "https://i.pinimg.com/originals/ce/34/dc/ce34dc3d5ec2c14892a1a3ff591ae589.jpg",
+        img: "Bring-Me-The-Horizon-Drown",
         src: "Bring Me The Horizon - Drown"
     },
     {
-        name: "Ikson Anywhere – Ikson",
-        artist: "Audio Library",
-        img: "music-2",
+        name: "Down",
+        artist: "Jason Walker",
+        img: "Cause-Of-Death",
         src: "Jason Walker - Down"
     },
     {
-        name: "Beauz & Jvna - Crazy",
-        artist: "Beauz & Jvna",
-        img: "music-3",
+        name: "The Only Exception",
+        artist: "Paramore",
+        img: "Cause-Of-Death",
         src: "Paramore - The Only Exception"
     },
     {
-        name: "Hardwind - Want Me",
-        artist: "Mike Archangelo",
-        img: "music-4",
+        name: "Face Down",
+        artist: "The Red Jumpsuit Apparatus",
+        img: "Cause-Of-Death",
         src: "The Red Jumpsuit Apparatus - Face Down"
     },
-    {
-        name: "Harley Bird - Home",
-        artist: "Jordan Schor",
-        img: "music-1",
-        src: "music-1"
-    },
-    {
-        name: "Ikson Anywhere – Ikson",
-        artist: "Audio Library",
-        img: "music-2",
-        src: "music-2"
-    },
-    {
-        name: "Beauz & Jvna - Crazy",
-        artist: "Beauz & Jvna",
-        img: "music-3",
-        src: "music-3"
-    },
-    {
-        name: "Hardwind - Want Me",
-        artist: "Mike Archangelo",
-        img: "music-4",
-        src: "music-4"
-    },
-    {
-        name: "Jim - Sun Goes Down",
-        artist: "Jim Yosef x Roy",
-        img: "music-5",
-        src: "music-5"
-    },
-    {
-        name: "Lost Sky - Vision NCS",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "music-6"
-    },
+    // {
+    //     name: "Harley Bird - Home",
+    //     artist: "Jordan Schor",
+    //     img: "music-1",
+    //     src: "music-1"
+    // },
+    // {
+    //     name: "Ikson Anywhere – Ikson",
+    //     artist: "Audio Library",
+    //     img: "music-2",
+    //     src: "music-2"
+    // },
+    // {
+    //     name: "Beauz & Jvna - Crazy",
+    //     artist: "Beauz & Jvna",
+    //     img: "music-3",
+    //     src: "music-3"
+    // },
+    // {
+    //     name: "Hardwind - Want Me",
+    //     artist: "Mike Archangelo",
+    //     img: "music-4",
+    //     src: "music-4"
+    // },
+    // {
+    //     name: "Jim - Sun Goes Down",
+    //     artist: "Jim Yosef x Roy",
+    //     img: "music-5",
+    //     src: "music-5"
+    // },
+    // {
+    //     name: "Lost Sky - Vision NCS",
+    //     artist: "NCS Release",
+    //     img: "music-6",
+    //     src: "music-6"
+    // },
 ];
