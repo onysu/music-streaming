aa5e7b2ea96c605c093b1be05b6c1a9e3832d2b1
ony <onydarrell@gmail.com>
2021-07-11 11:27:13 +0700

    M

--- player17/js/music-list.js
+++ player17/js/music-list.js
@@ -2,31 +2,31 @@ let allMusic = [{
         name: "Drown",
         artist: "Bring Me The Horizon",
         img: "Bring-Me-The-Horizon-Drown",
-        src: "Bring Me The Horizon - Drown"
+        src: "Bring-Me-The-Horizon-Drown"
     },
     {
         name: "Down",
         artist: "Jason Walker",
         img: "Jason Walker - Down",
-        src: "Jason Walker - Down"
+        src: "Jason-Walker-Down"
     },
     {
         name: "The Only Exception",
         artist: "Paramore",
         img: "Paramore - The Only Exception",
-        src: "Paramore - The Only Exception"
+        src: "Paramore-The-Only-Exception"
     },
     {
         name: "Face Down",
         artist: "The Red Jumpsuit Apparatus",
         img: "The Red Jumpsuit Apparatus - Face Down",
-        src: "The Red Jumpsuit Apparatus - Face Down"
+        src: "The-Red-Jumpsuit-Apparatus-Face-Down"
     },
     {
         name: "Kim",
         artist: "Eminem",
         img: "Eminem - Kim",
-        src: "Eminem - Kim"
+        src: "Eminem-Kim"
     },
     {
         name: "Harley Bird - Home",
