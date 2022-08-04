a917c62c51287a6dd93f15c7829621d0d1c59f4f
ony <onydarrell@gmail.com>
2022-08-04 21:53:58 +0700

    M

--- player18/music-list.js
+++ player18/music-list.js
@@ -1,31 +1,31 @@
 let allMusic = [{
-        name: "All By Myself",
-        artist: "CelineDion",
-        img: "All-By-Myself",
-        src: "All-By-Myself"
-    },
-    {
-        name: "A New Day Has Come",
-        artist: "CelineDion",
-        img: "A-New-Day-Has-Come",
-        src: "A-New-Day-Has-Come"
-    },
-    {
-        name: "Because You Loved Me",
-        artist: "CelineDion",
-        img: "Because-You-Loved-Me",
-        src: "Because-You-Loved-Me"
-    },
-    {
-        name: "Goodbyes (The Saddest Word)",
-        artist: "CelineDion",
-        img: "Goodbyes-(The Saddest Word)",
-        src: "Goodbyes-(The Saddest Word)"
-    },
-    {
-        name: "I am Alive",
-        artist: "CelineDion",
-        img: "I-am-Alive",
-        src: "I-am-Alive"
-    },
+  name: "All By Myself",
+  artist: "CelineDion",
+  img: "All-By-Myself",
+  src: "All-By-Myself"
+},
+{
+  name: "A New Day Has Come",
+  artist: "CelineDion",
+  img: "A-New-Day-Has-Come",
+  src: "A-New-Day-Has-Come"
+},
+{
+  name: "Because You Loved Me",
+  artist: "CelineDion",
+  img: "Because-You-Loved-Me",
+  src: "Because-You-Loved-Me"
+},
+{
+  name: "Goodbyes (The Saddest Word)",
+  artist: "CelineDion",
+  img: "Goodbyes-(The Saddest Word)",
+  src: "Goodbyes-(The Saddest Word)"
+},
+{
+  name: "I am Alive",
+  artist: "CelineDion",
+  img: "I-am-Alive",
+  src: "I-am-Alive"
+},
 ];
