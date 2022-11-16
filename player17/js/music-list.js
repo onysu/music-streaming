c80c3d7a1993e4ae83374d79849fa897cd77e5eb
ony <onydarrell@gmail.com>
2022-11-17 02:23:18 +0700

    M

--- player17/js/music-list.js
+++ player17/js/music-list.js
@@ -1,79 +1,19 @@
 let allMusic = [{
-  name: "A Feast Of Friends",
-  artist: "The Doors",
-  img: "The-Doors-A-Feast-Of-Friends",
-  src: "The-Doors-A-Feast-Of-Friends"
+  name: "Aku Tenang",
+  artist: "Fourtwnty",
+  img: "Fourtwnty-AkuTenang",
+  src: "Fourtwnty-AkuTenang"
 },
 {
-  name: "Drown",
-  artist: "Bring Me The Horizon",
-  img: "Bring-Me-The-Horizon-Drown",
-  src: "Bring-Me-The-Horizon-Drown"
+  name: "Zona Nyaman",
+  artist: "Fourtwnty",
+  img: "Fourtwnty-ZonaNyaman",
+  src: "Fourtwnty-ZonaNyaman"
 },
 {
-  name: "Face Down",
-  artist: "The Red Jumpsuit Apparatus",
-  img: "The-Red-Jumpsuit-Apparatus-Face-Down",
-  src: "The-Red-Jumpsuit-Apparatus-Face-Down"
+  name: "Untuk Perempuan",
+  artist: "Payung Teduh",
+  img: "PayungTeduh-Untuk Perempuan",
+  src: "PayungTeduh-Untuk Perempuan"
 },
-{
-  name: "Kim",
-  artist: "Eminem",
-  img: "Eminem-Kim",
-  src: "Eminem-Kim"
-},
-{
-  name: "Down",
-  artist: "Jason Walker",
-  img: "Jason-Walker-Down",
-  src: "Jason-Walker-Down"
-},
-{
-  name: "The Only Exception",
-  artist: "Paramore",
-  img: "Paramore-The-Only-Exception",
-  src: "Paramore-The-Only-Exception"
-},
-{
-  name: "In A Darkened Room",
-  artist: "Skid Row",
-  img: "Skid-Row-Monkey-Business",
-  src: "Skid-Row-In-A-Darkened-Room"
-},
-{
-  name: "Monkey Business",
-  artist: "Skid Row",
-  img: "skid-row",
-  src: "Skid-Row-Monkey-Business"
-},
-{
-  name: "Breakin Down",
-  artist: "Skid Row",
-  img: "Skid_Row_Breakin_Down_Part_one",
-  src: "Skid-Row-Breakin-Down"
-},
-{
-  name: "Symphony Of Destruction",
-  artist: "Megadeth",
-  img: "Megadeth-Symphony-Of-Destruction",
-  src: "Megadeth-Symphony-Of-Destruction"
-},
-{
-  name: "A Tout Le Monde",
-  artist: "Megadeth",
-  img: "Megadeth-A-Tout-Le-Monde",
-  src: "Megadeth-A-Tout-Le-Monde"
-},
-{
-  name: "A Secret Place",
-  artist: "Megadeth",
-  img: "Megadeth-A-Secret-Place",
-  src: "Megadeth-A-Secret-Place"
-},
-  // {
-  //     name: "Lost Sky - Vision NCS",
-  //     artist: "NCS Release",
-  //     img: "music-6",
-  //     src: "music-6"
-  // },
 ];
